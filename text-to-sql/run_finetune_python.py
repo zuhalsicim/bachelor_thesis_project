883efa5da1eaa9ad5b73e5729e4094c6b45cfe27
Sicim <Sicim@students.uni-marburg.de>
2025-08-28 17:26:35 +0200

    again x2 fine tune script error

--- text-to-sql/run_finetune_python.py
+++ text-to-sql/run_finetune_python.py
@@ -81,7 +81,6 @@ def main():
         model=model,
         train_dataset=dataset,
         peft_config=lora_config,
-        tokenizer=tokenizer,
         args=training_args,
     )
     print(" Trainer initialized. Starting fine-tuning...")
